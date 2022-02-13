046146f0e8604d3bdcf721efe614beec05acc77d
Eunsung Lim <unsungvegetable@gmail.com>
2022-02-13 18:38:00 +0900

    fix: use is None when None checking

--- groovy/config/settings/env.py
+++ groovy/config/settings/env.py
@@ -8,12 +8,12 @@ load_dotenv()
 def get_env_value(var_name, default=None):
     """help function for os.getenv to handle KeyError"""
     try:
-        if default != None:
+        if default is not None:
             return os.getenv(var_name, default=default)
         else:
             return os.getenv(var_name)
     except KeyError:
-        error_msg = "Set the {} environment variable".format(var_name)
+        error_msg = f"Set the {var_name} environment variable"
         raise ImproperlyConfigured(error_msg) from None
 
 
