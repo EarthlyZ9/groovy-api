e3f543b35ee27b150552e287cb3c338eeddcf3a4
Eunsung Lim <unsungvegetable@gmail.com>
2022-03-26 20:10:52 +0900

    fix: 상태검사 API

--- groovy/config/urls.py
+++ groovy/config/urls.py
@@ -3,10 +3,10 @@ from django.http import HttpResponse
 from django.urls import re_path, include, path
 
 
-def health_check_view():
+def health_check_view(request):
     return HttpResponse(status=200)
 
 urlpatterns = [
-    path("health-check/", health_check_view, name="health-check"),
+    path("health-check", health_check_view, name="health-check"),
     re_path(r"^v1/", include("config.urls_v1")),
 ]
