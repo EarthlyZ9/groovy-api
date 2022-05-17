6a10f3d50d69c2df1c817d399360ef2e6f00986f
linda2927 <89679621+linda2927@users.noreply.github.com>
2022-05-17 18:40:04 +0900

    style: change underbar to hyphen for url names (#42)

--- groovy/chat/urls.py
+++ groovy/chat/urls.py
@@ -7,15 +7,15 @@ from .views import (
 )
 
 urlpatterns = [
-    path("", GroupChatroomList.as_view(), name="group_chatroom_list"),
-    path("personal/", PersonalChatroomList.as_view(), name="personal_chatroom_list"),
+    path("", GroupChatroomList.as_view(), name="group-chatroom-list"),
+    path("personal/", PersonalChatroomList.as_view(), name="personal-chatroom-list"),
 
-    path("<int:pk>/", GroupChatList.as_view(), name="group_chat_list"),
-    path("personal/<int:pk>/", PersonalChatList.as_view(), name="personal_chat_list"),
+    path("<int:pk>/", GroupChatList.as_view(), name="group-chat-list"),
+    path("personal/<int:pk>/", PersonalChatList.as_view(), name="personal-chat-list"),
 
-    path("<int:pk>/notice/", GroupChatroomNoticeDetail.as_view(), name="group_chat_notice"),
-    path("personal/<int:pk>/notice/", PersonalChatroomNoticeDetail.as_view(), name="personal_chat_notice"),
+    path("<int:pk>/notice/", GroupChatroomNoticeDetail.as_view(), name="group-chat-notice"),
+    path("personal/<int:pk>/notice/", PersonalChatroomNoticeDetail.as_view(), name="personal-chat-notice"),
 
-    path("<int:pk>/chat/<int:chat_id>", PersonalChatDetail.as_view(), name="group_chat_detail"),
-    path("personal/<int:pk>/chat/<int:chat_id>", GroupChatDetail.as_view(), name="personal_chat_detail"),
+    path("<int:pk>/chat/<int:chat_id>", PersonalChatDetail.as_view(), name="group-chat-detail"),
+    path("personal/<int:pk>/chat/<int:chat_id>", GroupChatDetail.as_view(), name="personal-chat-detail"),
 ]
--- groovy/user/urls.py
+++ groovy/user/urls.py
@@ -5,14 +5,14 @@ from user import views
 from user.views import UniversityVerification
 
 urlpatterns = [
-    path("universities/", views.UniversityList.as_view(), name="university_list"),
+    path("universities/", views.UniversityList.as_view(), name="university-list"),
     path(
         "universities/<int:pk>/",
         views.UniversityDetail.as_view(),
-        name="university_detail",
+        name="university-detail",
     ),
-    path("", views.UserList.as_view(), name="user_list"),
-    path("<int:pk>/", views.UserDetail.as_view(), name="user_detail"),
-    path("<int:pk>/friend-request", SendFriendRequest.as_view(), name="send_friend_request"),
-    path("<int:pk>/verify-univ", UniversityVerification.as_view(), name="verify_univ"),
+    path("", views.UserList.as_view(), name="user-list"),
+    path("<int:pk>/", views.UserDetail.as_view(), name="user-detail"),
+    path("<int:pk>/friend-request", SendFriendRequest.as_view(), name="send-friend-request"),
+    path("<int:pk>/verify-univ", UniversityVerification.as_view(), name="verify-univ"),
 ]
