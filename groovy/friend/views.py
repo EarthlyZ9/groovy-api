428f133f04ea09e7c36862f933f39c6288cdaaf1
linda2927 <89679621+linda2927@users.noreply.github.com>
2022-05-17 18:29:28 +0900

    fix: filter friend request list against user (#41)

--- groovy/friend/views.py
+++ groovy/friend/views.py
@@ -5,20 +5,16 @@ from friend.models import Friend, FriendRequest
 from friend.serializers import FriendSerializer, FriendRequestSerializer
 from friend.services import FriendService
 from user.models import User
-from user.serializers import SimplifiedUserSerializer
 from user.services import NotificationService
 
 
 class FriendList(generics.GenericAPIView):
-    queryset = User.objects.all()
-    serializer_class = SimplifiedUserSerializer
+    queryset = Friend.objects.all()
+    serializer_class = FriendSerializer
 
-    def get(self, request):
-        user = request.user
-        queryset = self.get_queryset()
-        friends = FriendService.get_all_friends(queryset, user)
-        friends_data = self.serializer_class(friends, many=True).data
-        return Response(data=friends_data)
+    def get_queryset(self):
+        user = self.request.user
+        return FriendService.get_all_friends(self.queryset, user)
 
 
 class FriendDetail(generics.RetrieveDestroyAPIView):
@@ -37,26 +33,15 @@ class FriendDetail(generics.RetrieveDestroyAPIView):
 
 
 class FriendRequestList(generics.ListAPIView):
+    """
+    요청을 보낸 유저가 받은 friend request list
+    """
     queryset = FriendRequest.objects.all()
     serializer_class = FriendRequestSerializer
 
     def get_queryset(self):
         user = self.request.user
         return FriendService.get_all_friend_request(user=user)
-
-
-class SendFriendRequest(generics.CreateAPIView):
-
-    def create(self, request, *args, **kwargs):
-        request_from = request.user
-        request_to_id = kwargs.get("pk")
-        request_to = User.objects.get(pk=request_to_id)
-
-        friend_request, sent = FriendService.send_friend_request(request_from, request_to)
-        NotificationService.notify_friend_request(request_from, request_to)
-
-        if sent:
-            return Response(data=FriendRequestSerializer(friend_request).data)
 
 
 class FriendRequestDetail(generics.RetrieveUpdateDestroyAPIView):
@@ -79,3 +64,17 @@ class FriendRequestDetail(generics.RetrieveUpdateDestroyAPIView):
             )
 
         return Response(data=FriendRequestSerializer(updated_friend_request_obj).data)
+
+
+class SendFriendRequest(generics.CreateAPIView):
+
+    def create(self, request, *args, **kwargs):
+        request_from = request.user
+        request_to_id = kwargs.get("pk")
+        request_to = User.objects.get(pk=request_to_id)
+
+        friend_request, sent = FriendService.send_friend_request(request_from, request_to)
+        NotificationService.notify_friend_request(request_from, request_to)
+
+        if sent:
+            return Response(data=FriendRequestSerializer(friend_request).data, status=status.HTTP_201_CREATED)
